eb284bb5a534384fbf32a871d12e92f61fe55bf9
Edgar Talledos <onimenotsuki@gmail.com>
2018-03-17 16:08:56 -0600

    Refactoring styles only with glamorous

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,9 @@
 import glamorous from 'glamorous';
 import './Card.scss';
 
-export const Card = glamorous.div('mdc-card my-card');
+export const Card = glamorous.div('mdc-card', {
+  padding: '10px 20px',
+});
 export const CardMedia = glamorous.div('mdc-card__media');
 export const CardContent = glamorous.div('mdc_card__content');
 export const CardAction = glamorous.div('mdc-card__primary-action');
